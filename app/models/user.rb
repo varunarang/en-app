8821a8439612ab238ae76550ef1578fa4333437e
varunarang <varunarang@gmail.com>
2012-01-03 23:52:19 +0530

    uploading

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  
+  has_and_belongs_to_many :roles
+
   # Include default devise modules. Others available are:
   devise :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable , :omniauthable, :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
